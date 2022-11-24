4b6c8271c5bbc9438948538ac89221407e656736
FabioPatella <fabio140199@gmail.com>
2022-11-24 09:59:35 +0100

    update team choice in join tournament

--- frontend_ta/src/components/TournamentEntry.js
+++ frontend_ta/src/components/TournamentEntry.js
@@ -9,6 +9,7 @@ export class TournamentEntry extends React.Component {
         };
         this.DisplayTeamForm = this.DisplayTeamForm.bind(this)
         this.JoinButton = this.JoinButton.bind(this);
+        this.DisplayTournamentInfo=this.DisplayTournamentInfo.bind(this);
     }
     render() {
         let formtoshow;
@@ -16,7 +17,7 @@ export class TournamentEntry extends React.Component {
         else formtoshow = this.state.form;
         return (
             <div>
-                <div class="list-entry flex-container">
+                <div class="list-entry flex-container" onClick={this.DisplayTournamentInfo}>
                     <div class="col1 flex-items">{this.props.record.name}</div>
                     <div class="col2 flex-items">{this.props.record.date}</div>
                     <div class="col2 flex-items">{this.props.record.status}</div>
@@ -40,23 +41,35 @@ export class TournamentEntry extends React.Component {
             return <div className="btn" >Join</div>
         }
     }
+    DisplayTournamentInfo(){
+
+            let content;
+            content=<p>Content</p>
+            this.props.refreshView(this.props.viewindex);
+            this.setState({ form: content })
+    }
     DisplayTeamForm() {
 
-        let data = [
-            { "name": "Steaming hot coffee enjoyers" },
-            { "name": "Guys from the basement" }
-        ];
-        let formtodisplay = (
+        let data = 
+            { "name": "Steaming hot coffee enjoyers" };
+        let formtodisplay;
+        if(data!=null){
+         formtodisplay = (
             <div>
                 <form>
-                    <label forhtml="teamtojointour">Choose a team:</label>
-                    <select name="team" id="teamtojointour">
-                        {data.map((object, i) => <option record={object} key={i} value={object.name}>{object.name}</option>)}
-                    </select>
+                    <label forhtml="teamtojointour">Join with your team: {data.name}</label>
                 </form>
                 <input type="submit" value="Join Tournament" />
             </div>
         )
+        }
+        else{
+            formtodisplay = (
+                <div>
+                    <p>To join a tournament you must be owner of one team</p>
+                </div>
+            )
+        }
         this.props.refreshView(this.props.viewindex);
         this.setState({ form: formtodisplay })
 
