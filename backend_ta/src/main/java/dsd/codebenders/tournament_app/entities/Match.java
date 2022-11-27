e1bc69208e00e1ecc508d8eb8074f3b81f094fe8
ric00 <riccardo7.nava@mail.polimi.it>
2022-11-28 00:39:21 +0100

    Update Match entity

--- backend_ta/src/main/java/dsd/codebenders/tournament_app/entities/Match.java
+++ backend_ta/src/main/java/dsd/codebenders/tournament_app/entities/Match.java
@@ -1,8 +1,18 @@
 package dsd.codebenders.tournament_app.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import dsd.codebenders.tournament_app.entities.utils.MatchStatus;
 
-import javax.persistence.*;
 @Entity
 @Table(name = "game")
 public class Match {
@@ -12,16 +22,32 @@ public class Match {
     @Enumerated(EnumType.STRING)
     private MatchStatus status;
     private String server;
-    @Column(name="game_ID")
+    @Column(name = "game_ID")
     private Integer gameId;
-
+    @Column(name = "round_number", nullable = false)
+    private Integer roundNumber;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "tournament_id", nullable = false)
+    private Tournament tournament;
     @ManyToOne
     @JoinColumn(name = "ID_attackers_team")
     private Team attackersTeam;
-
     @ManyToOne
     @JoinColumn(name = "ID_defenders_team")
     private Team defendersTeam;
+    @ManyToOne
+    @JoinColumn(name = "winning_team_id")
+    private Team winningTeam;
+
+    public Match() {
+    }
+
+    public Match(Team attackersTeam, Team defendersTeam, Integer roundNumber, Tournament tournament) {
+        this.attackersTeam = attackersTeam;
+        this.defendersTeam = defendersTeam;
+        this.roundNumber = roundNumber;
+        this.tournament = tournament;
+    }
 
     public Long getID() {
         return ID;
@@ -39,6 +65,10 @@ public class Match {
         return gameId;
     }
 
+    public Tournament getTournament() {
+        return tournament;
+    }
+
     public Team getAttackersTeam() {
         return attackersTeam;
     }
@@ -46,4 +76,13 @@ public class Match {
     public Team getDefendersTeam() {
         return defendersTeam;
     }
+
+    public Team getWinningTeam() {
+        return winningTeam;
+    }
+
+    public void setWinningTeam(Team winningTeam) {
+        this.winningTeam = winningTeam;
+    }
+
 }
