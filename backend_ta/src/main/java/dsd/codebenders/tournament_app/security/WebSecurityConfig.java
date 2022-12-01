4c273d05fdfcb039e272c3c2ad0ec2d5a059432e
SimoneMezzaro <simone.mezzaro@mail.polimi.it>
2022-12-01 22:51:14 +0100

    Fix import error due to git rebase onto development branch

--- backend_ta/src/main/java/dsd/codebenders/tournament_app/security/WebSecurityConfig.java
+++ backend_ta/src/main/java/dsd/codebenders/tournament_app/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package dsd.codebenders.tournament_app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
