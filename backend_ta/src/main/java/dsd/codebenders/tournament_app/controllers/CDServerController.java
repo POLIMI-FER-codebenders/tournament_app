a99ef424090cb2e2f01cdd14561f112d3c243d7f
SimoneMezzaro <simone.mezzaro@mail.polimi.it>
2022-12-04 16:07:23 +0100

    Rename admin APIs

--- backend_ta/src/main/java/dsd/codebenders/tournament_app/controllers/CDServerController.java
+++ backend_ta/src/main/java/dsd/codebenders/tournament_app/controllers/CDServerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "admin/api/cd_server")
+@RequestMapping(path = "admin/api/cd-server")
 public class CDServerController {
 
     private final ServerService serverService;
