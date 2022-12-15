d805e89d70c8a0208dfa6d9125310dde5fba28d8
FabioPatella <fabio140199@gmail.com>
2022-12-15 22:03:38 +0100

    implemented back button on top of iframe and started timer

--- frontend_ta/src/components/CDFrame.js
+++ frontend_ta/src/components/CDFrame.js
@@ -3,12 +3,13 @@ import "../styles/iframe.css"
 import React from 'react';
 import { getData } from "../utils";
 import { GoToErrorPage } from "../utils";
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import clock from '../images/clock.png';
 
 export function CDFrame(props) {
 
-    const location = useLocation();
-    if(location.state==null) {
+  const location = useLocation();
+  /*  if(location.state==null) {
      return (<div>
      <p> you are not inside any game</p> 
      </div>);
@@ -17,7 +18,25 @@ export function CDFrame(props) {
           return  <iframe id="iframe" src={cdurl} title="cdtournamentsgames" allowFullScreen ></iframe>
             
     }
+    */
+  return (
+    <div id="maindiv">
+      
+         <div id="framemenu">
+          <a href="http://localhost:3000" id="backlink">Back To Tournament Application</a>
+          <img id="clock" src={clock}></img>
+          <p id="time" >Times</p>
+        
+      </div>
+      <div id="iframediv">
+        <iframe id="iframe" src="https://code-defenders.org" title="cdtournamentsgames" ></iframe>
+
+      </div>
+    </div>
+
+  );
 }
+
 
 
 
