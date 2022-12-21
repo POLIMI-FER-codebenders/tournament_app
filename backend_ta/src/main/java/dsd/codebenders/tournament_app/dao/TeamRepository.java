21861968d3112d6e72fa121e58e0f1cd16238ec8
hrvoje <62212065+hrvoje459@users.noreply.github.com>
2022-12-21 22:25:34 +0100

    Remove unneccesery import

--- backend_ta/src/main/java/dsd/codebenders/tournament_app/dao/TeamRepository.java
+++ backend_ta/src/main/java/dsd/codebenders/tournament_app/dao/TeamRepository.java
@@ -1,6 +1,6 @@
 package dsd.codebenders.tournament_app.dao;
 
-import dsd.codebenders.tournament_app.entities.Player;
+import java.util.List;
 import dsd.codebenders.tournament_app.entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
