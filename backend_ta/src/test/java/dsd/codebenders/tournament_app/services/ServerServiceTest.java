6d19758516f9c5b5fbfcfe4acafbc346508c0c3e
Andrea Restelli <andrearestelli28@gmail.com>
2023-01-03 18:02:56 +0100

    unit testing ServerService part 1

---
+++ backend_ta/src/test/java/dsd/codebenders/tournament_app/services/ServerServiceTest.java
@@ -0,0 +1,66 @@
+package dsd.codebenders.tournament_app.services;
+
+import dsd.codebenders.tournament_app.dao.ServerRepository;
+import dsd.codebenders.tournament_app.entities.CDPlayer;
+import dsd.codebenders.tournament_app.entities.Server;
+import dsd.codebenders.tournament_app.errors.CDServerUnreachableException;
+import dsd.codebenders.tournament_app.responses.LoadResponse;
+import dsd.codebenders.tournament_app.utils.HTTPRequestsSender;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@Transactional
+@AutoConfigureMockMvc
+class ServerServiceTest {
+
+    @Autowired
+    private ServerService serverService;
+
+    @MockBean
+    private ServerRepository serverRepository;
+
+    @Test
+    void getCDServer() throws CDServerUnreachableException {
+        // mock 4 servers with increasing load
+        Server server1 = Mockito.mock(Server.class);
+        Server server2 = Mockito.mock(Server.class);
+        Server server3 = Mockito.mock(Server.class);
+        Server server4 = Mockito.mock(Server.class);
+
+        Mockito.when(serverRepository.findAllActive()).thenReturn(Arrays.asList(server1, server2, server3, server4));
+
+        try (MockedStatic<HTTPRequestsSender> httpRequestsSenderMockedStatic = Mockito.mockStatic(HTTPRequestsSender.class)) {
+            LoadResponse loadResponse1 = new LoadResponse();
+            loadResponse1.setTotal(1);
+            LoadResponse loadResponse2 = new LoadResponse();
+            loadResponse2.setTotal(2);
+            LoadResponse loadResponse3 = new LoadResponse();
+            loadResponse3.setTotal(3);
+            LoadResponse loadResponse4 = new LoadResponse();
+            loadResponse4.setTotal(4);
+            httpRequestsSenderMockedStatic.when(() -> HTTPRequestsSender.sendGetRequest(Mockito.eq(server1), Mockito.eq("/admin/api/load"), Mockito.any())).thenReturn(loadResponse1);
+            httpRequestsSenderMockedStatic.when(() -> HTTPRequestsSender.sendGetRequest(Mockito.eq(server2), Mockito.eq("/admin/api/load"), Mockito.any())).thenReturn(loadResponse2);
+            httpRequestsSenderMockedStatic.when(() -> HTTPRequestsSender.sendGetRequest(Mockito.eq(server3), Mockito.eq("/admin/api/load"), Mockito.any())).thenReturn(loadResponse3);
+            httpRequestsSenderMockedStatic.when(() -> HTTPRequestsSender.sendGetRequest(Mockito.eq(server4), Mockito.eq("/admin/api/load"), Mockito.any())).thenReturn(loadResponse4);
+
+            assertEquals(server1, serverService.getCDServer());
+
+        }
+
+    }
+
+
+}
